d52d5c16625e30c8a09206ad27848e47af116e4f
Poós Dániel <poosdani@gmail.com>
2022-02-08 17:28:09 +0100

    added methods

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,29 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr v-for="statue in statues" v-bind:key="statue.id">
           <td>{{ statue.person }}</td>
           <td>{{ statue.height }}</td>
           <td>{{ statue.price }}</td>
           <td>
-            <button>Törlés</button>
-            <button>Szerkesztés</button>
+            <button @click="deleteStatue(statue.id)">Törlés</button>
+            <button @click="editStatue(statue.id)">Szerkesztés</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" v-model="statue.person">
+          </td>
+          <td>
+            <input type="number" v-model="statue.height">
+          </td>
+          <td>
+            <input type="number" v-model="statue.price">
+          </td>
+          <td>
+            <button v-if="mod_new" @click="newStatue" >Létrehoz</button>
+            <button v-if="!mod_new" @click="saveStatue" >Mentés</button>
+            <button v-if="!mod_new" @click="cancelEdit" >Mégse</button>
           </td>
         </tr>
       </tbody>
@@ -31,6 +47,7 @@ export default {
   },
   data() {
     return {
+      mod_new: true,
       statue: {
         id: null,
         person: '',
@@ -40,7 +57,66 @@ export default {
       statues: []
     }
   },
+  methods: {
+    async loadData () {
+     let Response = await fetch('http://127.0.0.1:8000/api/statues')
+     let data = await Response.json()
+     this.statues = data
+    },
+    async deleteStatue(id) {
+      await fetch(`http://127.0.0.1:8000/api/statues/${id}`, {
+        method: 'DELETE'
+      })
+      await this.loadData()
+    },
+    async newStatue() {
+     await fetch('http://127.0.0.1:8000/api/statues', {
+       method: 'POST',
+       headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },
+       body: JSON.stringify(this.statue) 
+     })
+     await this.loadData()
+     this.reset()
+    },
+    async saveStatue() {
+     await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
+       method: 'PATCH',
+       headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },
+       body: JSON.stringify(this.statue) 
+     })
+     await this.loadData()
+     this.reset()
+    },
+    async editStatue(id) {
+      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
+      let data = await Response.json()
+      this.statue = {...data};
+      this.mod_new = false
+    },
+    cancelEdit () {
+      this.reset()
+    },
+    reset() {
+      this.statue = {
+        id: null,
+        person: '',
+        height: '',
+        price: false
+      }
+      this.mod_new = true
+    }
+  },
+  mounted() {
+    this.loadData()
+  }
 }
 </script>
 <style>
+
 </style>
